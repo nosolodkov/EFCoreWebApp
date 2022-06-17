f5312b2a5f30e61f6f6b8582cbb85ab38c5a584e
Nikolay Osolodkov <nnosolodkov@bk.ru>
2022-06-17 11:47:08 +0300

    Add project files.

---
+++ tests/EFCoreWebIntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EFCoreWebIntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/EFCoreWebUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EFCoreWebUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
